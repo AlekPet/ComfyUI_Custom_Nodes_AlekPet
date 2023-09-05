4f58e685c1ff9932c451016830550dd169fc6a59
AlekPet <alexepetrof@gmail.com>
2023-09-05 15:27:18 +0300

    fix

--- ExtrasNode/js/extras_node.js
+++ ExtrasNode/js/extras_node.js
@@ -1,12 +1,12 @@
-import { app } from "../../scripts/app.js";
-import { ComfyWidgets } from "../../scripts/widgets.js";
+import { app } from "/scripts/app.js";
+import { ComfyWidgets } from "/scripts/widgets.js";
 
 app.registerExtension({
   name: "Comfy.ExtrasNode",
   async beforeRegisterNodeDef(nodeType, nodeData, app) {
     if (nodeData.name === "PreviewTextNode") {
+      // Node Created
       const onNodeCreated = nodeType.prototype.onNodeCreated;
-
       nodeType.prototype.onNodeCreated = function () {
         const r = onNodeCreated
           ? onNodeCreated.apply(this, arguments)
@@ -32,13 +32,17 @@ app.registerExtension({
           ],
           app
         );
-        this.onDrawBackground = function () {
-          const output = app.nodeOutputs[this.id + ""];
-          if (output && output.string) {
-            this.widgets[0].inputEl.value = output.string[0];
-          }
-        };
         return r;
+      };
+      // Node Executed
+      const onNodeExecuted = nodeType.prototype.onExecuted;
+      nodeType.prototype.onExecuted = function () {
+        onNodeExecuted?.apply(this, arguments);
+
+        const output = app.nodeOutputs[this.id + ""];
+        if (output && output.string) {
+          this.widgets[0].inputEl.value = output.string[0];
+        }
       };
     }
   },
