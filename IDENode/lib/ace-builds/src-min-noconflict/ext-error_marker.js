d8629270c944f2328d890d03a6a73e14c428ef75
AlekPet <alexepetrof@gmail.com>
2024-07-22 12:45:28 +0300

    fix error browser console log ide node syntax highlighting

--- IDENode/lib/ace-builds/src-min-noconflict/ext-error_marker.js
+++ IDENode/lib/ace-builds/src-min-noconflict/ext-error_marker.js
@@ -1,4 +1,4 @@
-;                (function() {
+import "./ace.js";                (function() {
                     ace.require(["ace/ext/error_marker"], function(m) {
                         if (typeof module == "object" && typeof exports == "object" && module) {
                             module.exports = m;
