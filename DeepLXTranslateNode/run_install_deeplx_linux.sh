fbce57d86acc3ae365312a3f43e558944446db1c
AlekPet <alexepetrof@gmail.com>
2025-07-10 13:23:14 +0300

    removed DeepLX nodes, service no longer works...

--- DeepLXTranslateNode/run_install_deeplx_linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Install Golang and DeepLX for ComfyUI DeepLXTranslateNode by AlekPet
-# Github repository: https://github.com/AlekPet/ComfyUI_Custom_Nodes_AlekPet/tree/master/DeepLXTranslateNode
-
-python install_deeplx.py
